995cb43224e783a34afe87cff58a05a72fbda462
malyroiyan <112873566+malyroiyan@users.noreply.github.com>
2022-09-13 13:21:37 +0700

    Add files via upload

---
+++ Tugas 1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "NAMA        :M.ALY ROIYAN"<< endl;
+    cout << "NIM         :A11.2022.14662"<<endl;
+    cout << "ASAL SEKOLAH:SMA N1 BANGSRI"<<endl;
+
+    return 0;
+}

bb57abd510ee010ad8c1c69f3d95cfeec4251c55
malyroiyan <112873566+malyroiyan@users.noreply.github.com>
2022-09-13 13:55:00 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+        //insatalasi
+        int x =2022;
+        int b =19;
+        int c =170;
+        float t =170;
+        float g =11.14662;
+        string a ="M.ALY ROIYAN";
+        string d ="GAME,MENGGEMBALA KAMBING";
+
+        //hasil keluar
+        cout << "Hello world!"<< x;
+        cout << "\nNAMA      : " <<a;
+        cout << "\nUMUR      : " <<b;
+        cout << "\nTINGGI    : " <<c;
+        cout << "\nHOBI      : " <<d;
+
+
+        printf("\n\nTest");
+        return 0;
+}
+
+
+
